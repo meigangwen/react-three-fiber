7cba36b74ae0727478c932ed3fa7db948277aac5
fancilivain <meigangwen@gmail.com>
2023-08-02 19:58:39 +0800

    updated with useFrame

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-'use client'
-
 import { StrictMode } from 'react'
-//import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-
-function Box() {
-  return (
-    <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe />
-    </mesh>
-  )
-}
+import App from '@/components/App'
 
 export default function Home() {
   return (
     <StrictMode>
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <Box />
-      </Canvas>
+      <App />
     </StrictMode>
   )
 }
