94809470ee0fa08017ccef69cfcacf3fc684c423
fancilivain <meigangwen@gmail.com>
2023-08-04 00:17:56 +0800

    finished the loader scene

--- components/App.tsx
+++ components/App.tsx
@@ -1,18 +1,21 @@
 'use client'
 
-import { Stats, OrbitControls } from '@react-three/drei'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { Stats, OrbitControls, ContactShadows } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import Env from '@/components/Env'
+import Model from '@/components/Model'
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, './models/scene.glb')
-
   return (
     <Canvas camera={{ position: [-8, 5, 8] }}>
       <Env />
       <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
-      <primitive object={gltf.scene} />
+      <Model />
+      <ContactShadows
+        scale ={150}
+        position = {[0.33,-0.33,0.33]}
+        opacity = {1.5} 
+      />
       <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
       <axesHelper args={[5]} />
       <Stats />
