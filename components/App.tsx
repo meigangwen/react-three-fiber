d4d171f5d35c0d644b1461718b78583214d08ca2
fancilivain <meigangwen@gmail.com>
2023-08-03 10:56:32 +0800

    sharing objects

--- components/App.tsx
+++ components/App.tsx
@@ -1,13 +1,22 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import Box from '@/components/Box'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
 
 export default function App() {
-    return (
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <Box position={[-0.75,0,0]} name="A" />
-        <Box position={[0.75,0,0]} name="B" />
-      </Canvas>
-    )
-  }
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ]
+
+  return (
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    </Canvas>
+  )
+}
--- components/Box.tsx
+++ components/Box.tsx
@@ -1,30 +1,32 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect, useMemo } from 'react'
 import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
-export default function Box(props:any) {
-    const ref = useRef()
-    const [hovered, setHover] = useState(false)
-    const [rotate, setRotate] = useState(false)
+export default function Box(props) {
+  const ref = useRef()
+  const [count, setCount] = useState(0)
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398)],
+    []
+  )
 
-    useFrame((_,delta) => {
-      if (rotate) {
-        ref.current.rotation.x += 1 * delta
-        ref.current.rotation.y += 0.5 * delta
-      }
-    })
+  useEffect(() => {
+    console.log(ref.current.geometry.uuid)
+  })
 
-    return (
-      <mesh 
-        {...props} 
-        ref={ref}
-        scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
-        onPointerDown={() => setRotate(!rotate)}
-        //onPointerUp={(e) => console.log("Pointer up " + e.object.name)}
-        onPointerOver={() => setHover(true)}
-        onPointerOut={() => setHover(false)}
-        onUpdate={(self) => console.log(self)}>
-          <boxGeometry />
-          <meshBasicMaterial color={hovered? 0xff0000 : 0x00ff00} wireframe />
-      </mesh>
-    )
+  useFrame((_, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={() => setCount((count + 1) % 2)}
+      geometry={geometry[count]}
+    >
+      <meshBasicMaterial color={'lime'} wireframe />
+    </mesh>
+  )
 }
