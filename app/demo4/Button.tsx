b4c31acd892b4cd90ddf6358321c6c73a887ce82
fancilivain <meigangwen@gmail.com>
2023-08-05 21:50:44 +0800

    added interaction code

--- app/demo4/Button.tsx
+++ app/demo4/Button.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import { MathUtils } from 'three'
 
-export default function Button(props) {
-    const [hovered, setHovered] = useState(false)
+export default function Button(props:any) {
+    const ref = useRef()
+    const [hovered, setHovered] = useState()
+
+    useFrame(() => {
+        ref.current.rotation.x = hovered 
+            ? MathUtils.lerp( ref.current.rotation.x, -Math.PI * 2, 0.025)
+            : MathUtils.lerp( ref.current.rotation.x, 0 , 0.025)
+    })
+
     return (
         <mesh 
             {...props}
-        >
+            onPointerOver={() => setHovered(true)}
+            onPointerOut={() => setHovered(false)}>
             <icosahedronGeometry />
             <meshPhysicalMaterial
                 roughness={0}
