faba861c00c9c66c959aaedfe3c90d07240def2a
fancilivain <meigangwen@gmail.com>
2023-08-06 22:19:06 +0800

    finished react fiber course

---
+++ app/demo5/Box.tsx
@@ -0,0 +1,22 @@
+import { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Box(props) {
+  const ref = useRef()
+  const keyMap = props.keyMap
+  const [selected, setSelected] = useState(props.selected)
+
+  useFrame((_, delta) => {
+    keyMap['KeyA'] && selected && (ref.current.position.x -= 1 * delta)
+    keyMap['KeyD'] && selected && (ref.current.position.x += 1 * delta)
+    keyMap['KeyW'] && selected && (ref.current.position.z -= 1 * delta)
+    keyMap['KeyS'] && selected && (ref.current.position.z += 1 * delta)
+  })
+
+  return (
+    <mesh ref={ref} {...props} onPointerDown={() => setSelected(!selected)}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe={!selected} />
+    </mesh>
+  )
+}
