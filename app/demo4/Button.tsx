fcac300173489ac0dde263966884ff2497831057
fancilivain <meigangwen@gmail.com>
2023-08-06 21:03:55 +0800

    finished lerp

--- app/demo4/Button.tsx
+++ app/demo4/Button.tsx
@@ -1,17 +1,31 @@
 import { useFrame } from '@react-three/fiber'
-import { useRef, useState } from 'react'
-import { MathUtils,Mesh } from 'three'
+import { useRef, useState, useMemo } from 'react'
+import { MathUtils,Color } from 'three'
 
+const black = new Color('black')
 export default function Button(props:any) {
     const ref = useRef()
     const [hovered, setHovered] = useState(false)
+    const [selected, setSelected] = useState(false)
+
     const currentMesh = ref.current;
+
+    const colorTo = useMemo(
+        () => new Color(Math.floor(Math.random() * 16777216)),
+        []
+    )
   
     useFrame(() => {
         if (currentMesh != undefined) {
             currentMesh.rotation.x = hovered 
                 ? MathUtils.lerp( currentMesh.rotation.x, -Math.PI * 2, 0.025) 
                 : MathUtils.lerp( currentMesh.rotation.x, 0 , 0.025)
+
+            currentMesh.position.z = selected
+                ? MathUtils.lerp(currentMesh.position.z, 0, 0.025)
+                : MathUtils.lerp(currentMesh.position.z, -3, 0.025)
+            
+            currentMesh.material.color.lerp(selected ? colorTo : black, 0.025)
         }
     })
 
@@ -19,6 +33,7 @@ export default function Button(props:any) {
         <mesh 
             {...props}
             ref = {ref}
+            onPointerDown={() => setSelected(!selected)}
             onPointerOver={() => setHovered(true)}
             onPointerOut={() => setHovered(false)}>
             <icosahedronGeometry />
